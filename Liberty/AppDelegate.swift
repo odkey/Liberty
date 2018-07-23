9e455fcea959eb4e009b34b5661b37c96462bf62
Yota Odaka <amglowez@gmail.com>
2018-07-23 17:34:05 +0900

    Initial Commit

---
+++ Liberty/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  Liberty
+//
+//  Created by Yota Odaka on 2018/07/23.
+//  Copyright © 2018 Yota Odaka. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
